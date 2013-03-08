62e25205bb158888be506cf8cee82b982d98cdca
btanders@asu.edu <btanders@asu.edu@e47d3464-92e7-4361-f94d-ccb9854d7d43>
2013-03-08 20:13:40 +0000

    Final commit... I hope.

--- POX-FoodMenu-btanders-Netbeans-7.2.1/src/main/java/com/asu/cse598/btanders/poxfoodmenubtandersnetbeans7/TestClient.java
+++ POX-FoodMenu-btanders-Netbeans-7.2.1/src/main/java/com/asu/cse598/btanders/poxfoodmenubtandersnetbeans7/TestClient.java
@@ -9,6 +9,11 @@
  * 
  * Assignment - POX over HTTP
  * 
+ * TestClient
+ * 
+ * Simple Java program with a main method that instantiates the web service
+ * food items proxy and makes various add and get requests, printing the results
+ * to the console or output pane on Netbeans.
  */
 package com.asu.cse598.btanders.poxfoodmenubtandersnetbeans7;
 
@@ -25,39 +30,91 @@ public class TestClient
     {
         FoodItemsProxy foodProxy = new FoodItemsProxy();
 
-        String addFoodItem = "";
+        String addFoodItem1 = "";
+        String addFoodItem2 = "";
+        String addFoodItem3 = "";
 
-        addFoodItem += "<NewFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
-        addFoodItem += "<FoodItem country=\"G\">";
-        addFoodItem += "<name>Cornish Pasty</name>";
-        addFoodItem += "<description>Tender cubes of steak, potatoes and sweede wrapped in flakey short crust pastry.  Seasoned with lots of pepper.  Served with mashed potatoes, peas and a side of gravy</description>";
-        addFoodItem += "<category>Dinner</category>";
-        addFoodItem += "<price>15.95</price>";
-        addFoodItem += "</FoodItem>";
-//        addFoodItem += "<FoodItem country=\"GB\">";
-//        addFoodItem += "<name>bangers and mash</name>";
-//        addFoodItem += "<description>the yumzors!!!</description>";
-//        addFoodItem += "<category>anytime</category>";
-//        addFoodItem += "<price>9.99</price>";
-//        addFoodItem += "</FoodItem>";
-        addFoodItem += "</NewFoodItems >";
+        // A new food item to add...
+        addFoodItem1 += "<NewFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        addFoodItem1 += "<FoodItem country=\"MX\">";
+        addFoodItem1 += "<name>Taco</name>";
+        addFoodItem1 += "<description>A traditional Mexican dish usually made with corn or flour tortillas containing a variety of fillings.</description>";
+        addFoodItem1 += "<category>Lunch</category>";
+        addFoodItem1 += "<price>2.00</price>";
+        addFoodItem1 += "</FoodItem>";
+        addFoodItem1 += "</NewFoodItems >";
 
-        String getFoodItem = "";
+        // A repeat of the item above to see if we can trick the system into inserting it twice...
+        addFoodItem2 += "<NewFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        addFoodItem2 += "<FoodItem country=\"MX\">";
+        addFoodItem2 += "<name>Taco</name>";
+        addFoodItem2 += "<description>A traditional Mexican dish usually made with corn or flour tortillas containing a variety of fillings.</description>";
+        addFoodItem2 += "<category>Lunch</category>";
+        addFoodItem2 += "<price>2.00</price>";
+        addFoodItem2 += "</FoodItem>";
+        addFoodItem2 += "</NewFoodItems >";
 
-        getFoodItem += "<SelectedFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
-        getFoodItem += "<FoodItemId>100</FoodItemId>";
-        getFoodItem += "<FoodItemId>156</FoodItemId>";
-        getFoodItem += "<FoodItemId>302</FoodItemId>";
-        getFoodItem += "</SelectedFoodItems >";
+        // Another new dish, but with malformed XML...
+        addFoodItem3 += "<NewFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        addFoodItem3 += "<FoodItem country=\"IN\">";
+        addFoodItem3 += "<name>Dal makhani"; // missing the closing tag for the name... too bad since this dish is tasty
+        addFoodItem3 += "<description>Whole black lentil and red kidney beans in a ruch sauce</description>";
+        addFoodItem3 += "<category>Dinner</category>";
+        addFoodItem3 += "<price>9.99</price>";
+        addFoodItem3 += "</FoodItem>";
+        addFoodItem3 += "</NewFoodItems >";
+
+        String getFoodItem1 = "";
+        String getFoodItem2 = "";
+        String getFoodItem3 = "";
+
+        // A get request using an XML message, should succeed with 200...
+        getFoodItem1 += "<SelectedFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        getFoodItem1 += "<FoodItemId>100</FoodItemId>"; // Steak and Kidney Pie
+        getFoodItem1 += "<FoodItemId>105</FoodItemId>"; // Traditional English Breakfast
+        getFoodItem1 += "<FoodItemId>302</FoodItemId>"; // This should be the item we added above, Taco...
+        getFoodItem1 += "</SelectedFoodItems >";
+
+        // A get request using an XML message, this one should partially succeed with 404...
+        getFoodItem2 += "<SelectedFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        getFoodItem2 += "<FoodItemId>999</FoodItemId>"; // Should not exist so an invalid food item...
+        getFoodItem2 += "<FoodItemId>100</FoodItemId>"; // Steak and Kidney Pie
+        getFoodItem2 += "</SelectedFoodItems >";
+
+        // A get request using an XML message, this one won't succeed -> 404...
+        getFoodItem3 += "<SelectedFoodItems xmlns=”http://cse460.asu.edu/PoxAssignment”>";
+        getFoodItem3 += "<FoodItemId>999</FoodItemId>"; // Should notr exist so an invalid food item...
+        getFoodItem3 += "</SelectedFoodItems >";
+
+        // This one is for the URI tunneling version of the get request, should succeed with 200...
+        ArrayList<String> foodItemIdList1 = new ArrayList<String>();
+        foodItemIdList1.add("100"); // Steak and Kidney Pie
+        foodItemIdList1.add("105"); // Traditional English Breakfast
+        foodItemIdList1.add("302"); // This should be the item we added above, Taco...
+
+        // This one is for the URI tunneling version of the get request, this one should partially succeed with 404...
+        ArrayList<String> foodItemIdList2 = new ArrayList<String>();
+        foodItemIdList2.add("999"); // Should not exist so an invalid food item...
+        foodItemIdList2.add("100"); // Steak and Kidney Pie
+
+        // This one is for the URI tunneling version of the get request, this one won't succeed -> 404...
+        ArrayList<String> foodItemIdList3 = new ArrayList<String>();
+        foodItemIdList3.add("999"); // Should notr exist so an invalid food item...
 
         try
         {
-
-            ClientResponse c = foodProxy.addFoodItem(addFoodItem);
+            System.out.println("\nAdding a new food item, should succeed with 200 OK...\n");
+            ClientResponse c = foodProxy.addFoodItem(addFoodItem1);
             System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
             System.out.println(c.getEntity(String.class));
 
-            c = foodProxy.addFoodItem(addFoodItem);
+            System.out.println("\nAdding the same food item again, should not succedd -> 409 Conflict...\n");
+            c = foodProxy.addFoodItem(addFoodItem2);
+            System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
+            System.out.println(c.getEntity(String.class));
+
+            System.out.println("\nAdding a new food item with malformed XML, should not succedd -> 400 Invalid or incorrect input message...\n");
+            c = foodProxy.addFoodItem(addFoodItem3);
             System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
             System.out.println(c.getEntity(String.class));
         }
@@ -68,17 +125,37 @@ public class TestClient
 
         try
         {
-            ClientResponse c = foodProxy.getFoodItemByPost(getFoodItem);
+            // POX over HTTP version of get request...
+            System.out.println("\nTime to make some POX over HTTP get requests...\n");
+            System.out.println("\nGetting three food items, should succeed with 200 OK\n");
+            ClientResponse c = foodProxy.getFoodItemByPost(getFoodItem1);
+            System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
+            System.out.println(c.getEntity(String.class));
+            
+            System.out.println("\nGetting two food items, the first of which does not exist, should partially succeed with 404 Not Found\n");
+            c = foodProxy.getFoodItemByPost(getFoodItem2);
+            System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
+            System.out.println(c.getEntity(String.class));
+            
+            System.out.println("\nGetting one food item that does not exist, should not succeed -> 404 Not Found\n");
+            c = foodProxy.getFoodItemByPost(getFoodItem3);
             System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
             System.out.println(c.getEntity(String.class));
 
-            ArrayList<String> foodItemIdList = new ArrayList<String>();
-
-            foodItemIdList.add("100");
-            foodItemIdList.add("101");
-            foodItemIdList.add("302");
-
-            c = foodProxy.getFoodItem(ClientResponse.class, foodItemIdList);
+            // URI tunneling version of get request
+            System.out.println("\nNow we will make get requests (the same as above) but using URI tunneling...\n");
+            System.out.println("\nGetting three food items, should succeed with 200 OK\n");
+            c = foodProxy.getFoodItem(ClientResponse.class, foodItemIdList1);
+            System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
+            System.out.println(c.getEntity(String.class));
+            
+            System.out.println("\nGetting two food items, the first of which does not exist, should partially succeed with 404 Not Found\n");
+            c = foodProxy.getFoodItem(ClientResponse.class, foodItemIdList2);
+            System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
+            System.out.println(c.getEntity(String.class));
+            
+            System.out.println("\nGetting one food item that does not exist, should not succeed -> 404 Not Found\n");
+            c = foodProxy.getFoodItem(ClientResponse.class, foodItemIdList3);
             System.out.println(c.getStatus() + ": " + c.getClientResponseStatus());
             System.out.println(c.getEntity(String.class));
         }
@@ -86,7 +163,9 @@ public class TestClient
         {
             System.out.println(e.getMessage());
         }
-        
+
         foodProxy.close();
+        
+        System.out.println("\nThe client is exiting...\n");
     }
 }
